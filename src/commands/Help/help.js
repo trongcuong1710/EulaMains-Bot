24f7a86375828becd2cae0d7e8961ba7b1cf135b
Zyla <erencana12@gmail.com>
2021-06-02 00:57:58 +0300

    troll help command

--- src/commands/Help/help.js
+++ src/commands/Help/help.js
@@ -23,8 +23,10 @@ class HelpCommand extends Command {
     if (!args.command)
       return message.channel.send(
         new Discord.MessageEmbed({
-          color: 'RED',
-          description: `Please specify a command to view more info about.`,
+          color: 'BLUE',
+          description: `You expected me to show a list of commands with ${
+            prefix + this.id
+          }?\nWrong, you get my ko-fi page instead.\nhttps://ko-fi.com/zylasden.`,
         })
       );
 
